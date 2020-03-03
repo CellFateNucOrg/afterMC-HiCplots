bd6a5b4fa960cfb04d597ee453458115136e9a32
Moushumi Das (external) <mdas@izblisbon.campus.unibe.ch>
2020-03-03 13:05:04 +0100

    different script and work dirs

--- runCoolByDomain.sh
+++ runCoolByDomain.sh
@@ -12,8 +12,9 @@
 source ${CONDA_ACTIVATE} MC-HiC-env
 echo "conda activate is: " ${CONDA_ACTIVATE}
 
-workDIR=.
-coolFile=${workDIR}/mcool/combine_TEVneg_HIC_8_12.8_bw5kb.50000.cool
+workDIR=..
+scriptDIR=.
+coolFile=${workDIR}/mcool/combine_TEVpos_HIC_10_12.5_13_bw5kb.50000.cool
 domains=( left center right )
 binSize=50000
 
@@ -23,13 +24,13 @@ baseFileName=${baseFileName%5kb.${binSize}.cool}50kb
 echo "base file name: " $baseFileName
 
 chrDomainDIR=${workDIR}/chrDomain
-mkdir -p chrDomain
+mkdir -p ${chrDomainDIR}
 
 for domain in ${domains[@]}
 do
-  ranges=(`cut -f 1 ${workDIR}/${domain}.chrom.ranges`)
-  pseudoChr=(`cut -f 2 ${workDIR}/${domain}.chrom.ranges`)
-  roundedStart=(`cut -f 3 ${workDIR}/${domain}.chrom.ranges`)
+  ranges=(`cut -f 1 ${scriptDIR}/${domain}.chrom.ranges`)
+  pseudoChr=(`cut -f 2 ${scriptDIR}/${domain}.chrom.ranges`)
+  roundedStart=(`cut -f 3 ${scriptDIR}/${domain}.chrom.ranges`)
 
   let maxIndex=${#ranges[@]}-1
   
@@ -47,8 +48,6 @@ do
 
     echo "${pseudoChr[$i]} ${domain} dumped"
 
-    #awk -F'\t' 'BEGIN {OFS=FS} {$1='${pseudoChr[$i]}'} {$4='${pseudoChr[$i]}'} {print}'  ${chrDomainDIR}/${baseFileName}_${pseudoChr[$i]}_tmp.bg2  >  ${chrDomainDIR}/${baseFileName}_${pseudoChr[$i]}.bg2
-
     awk -F'\t' -v chr=${pseudoChr[$i]} -v start=${roundedStart[$i]} 'BEGIN {OFS=FS} {$1=chr} {$2=$2-start} {$3=$3-start} {$4=chr} {$5=$5-start} {$6=$6-start} {print}'  ${chrDomainDIR}/${baseFileName}_${pseudoChr[$i]}_${domain}_tmp.bg2  >  ${chrDomainDIR}/${baseFileName}_${pseudoChr[$i]}_${domain}.bg2
 
 
@@ -59,7 +58,7 @@ do
 
   rm  ${chrDomainDIR}/${baseFileName}_chr*_${domain}*.bg2
 
-  cooler load -f bg2 ${workDIR}/${domain}.chrom.sizes:50000 ${chrDomainDIR}/${baseFileName}_${domain}Arm.bg2 ${chrDomainDIR}/${baseFileName}_${domain}Arm.cool
+  cooler load -f bg2 ${scriptDIR}/${domain}.chrom.sizes:50000 ${chrDomainDIR}/${baseFileName}_${domain}Arm.bg2 ${chrDomainDIR}/${baseFileName}_${domain}Arm.cool
 
 done
 
@@ -71,13 +70,13 @@ chrNames=( chrI chrII chrIII chrIV chrV chrX )
   # do decay plots
   #################
   # plotted per chr
-  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile decayPlots/DistVsCounts_${baseFileName}_domains_perChr.pdf --perchr --skipDiagonal --maxdepth 20000000 --plotsize 6 5 --labels "center" "left" "right"
+  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile ${workDIR}/decayPlots/DistVsCounts_${baseFileName}_domains_perChr.pdf --perchr --skipDiagonal --maxdepth 20000000 --plotsize 6 5 --labels "center" "left" "right"
 
   # plotted all chr together
-  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile decayPlots/DistVsCounts_${baseFileName}_domains.pdf --skipDiagonal --maxdepth 20000000 --plotsize 6 5 --labels "center" "left" "right"
+  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile ${workDIR}/decayPlots/DistVsCounts_${baseFileName}_domains.pdf --skipDiagonal --maxdepth 20000000 --plotsize 6 5 --labels "center" "left" "right"
 
   # autosomes only
-  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile decayPlots/DistVsCounts_${baseFileName}_domains_autosomes.pdf --skipDiagonal --maxdepth 20000000 --chromosomeExclude chrX --plotsize 6 5 --labels "center" "left" "right"
+  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile ${workDIR}/decayPlots/DistVsCounts_${baseFileName}_domains_autosomes.pdf --skipDiagonal --maxdepth 20000000 --chromosomeExclude chrX --plotsize 6 5 --labels "center" "left" "right"
 
   # Xchr only
-  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile decayPlots/DistVsCounts_${baseFileName}_domains_Xchr.pdf --skipDiagonal --maxdepth 20000000 --chromosomeExclude chrI chrII chrIII chrIV chrV --plotsize 6 5 --labels "center" "left" "right"
+  hicPlotDistVsCounts --matrices ${coolFiles[@]} --plotFile ${workDIR}/decayPlots/DistVsCounts_${baseFileName}_domains_Xchr.pdf --skipDiagonal --maxdepth 20000000 --chromosomeExclude chrI chrII chrIII chrIV chrV --plotsize 6 5 --labels "center" "left" "right"
